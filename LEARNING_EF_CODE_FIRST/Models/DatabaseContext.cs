b81ad6a8e18e2bcf170aa56080e3e429e358ef01
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-09-06 10:58:53 +0430

    Add project files.

---
+++ LEARNING_EF_CODE_FIRST/Models/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Models
+{
+	public class DatabaseContext : System.Data.Entity.DbContext
+	{
+		static DatabaseContext()
+		{
+			// اين گزينه به درد برنامه نويسان می خورد
+			//System.Data.Entity.Database.SetInitializer
+			//	(new System.Data.Entity.DropCreateDatabaseAlways<DatabaseContext>());
+
+			// اين گزينه به درد برنامه نويسان می خورد
+			System.Data.Entity.Database.SetInitializer
+				(new System.Data.Entity.DropCreateDatabaseIfModelChanges<DatabaseContext>());
+
+			// اين گزينه به درد مشتری می خورد
+			//System.Data.Entity.Database.SetInitializer
+			//	(new System.Data.Entity.CreateDatabaseIfNotExists<DatabaseContext>());
+		}
+
+		public DatabaseContext() : base()
+		{
+		}
+
+		public System.Data.Entity.DbSet<Person> People { get; set; }
+	}
+}
